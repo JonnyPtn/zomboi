eae02f6c5ebb96f8389cb58d7f890d352d9fd32f
Gilbert Gomez <gomezgilbert786@gmail.com>
2023-07-08 14:32:24 -0400

    Added shutdown message for discord text channel

--- admin.py
+++ admin.py
@@ -14,23 +14,29 @@ class AdminLogHandler(commands.Cog):
     """
 
     def __init__(self, bot, logPath):
+        self.was_turned_off = False
         self.bot = bot
         self.logPath = logPath
         self.lastUpdateTimestamp = datetime.now()
         self.sendLogs = os.getenv("ADMIN_LOGS", "True") == "True"
         # If the user has not enabled Admin logs, let's exit
         if not self.sendLogs:
+            self.bot.log.info("Admin logs not enabled.")
             return 
         self.adminChannel = os.getenv("ADMIN_CHANNEL")
+        self.text_channel = os.getenv("CHANNEL")
         if not self.adminChannel:
             self.bot.log.warning("Unable to get admin channel, setting to default channel...")
             self.adminChannel = self.bot.channel.name
         if self.adminChannel.isdigit():
             self.adminChannel = self.bot.get_channel(int(self.adminChannel))  # Find by id
+        if self.text_channel.isdigit():
+            self.text_channel = self.bot.get_channel(int(self.text_channel))  # Find by id
         if isinstance(self.adminChannel, str):
             self.adminChannel = discord.utils.get(
                 self.bot.get_all_channels(), name=self.adminChannel
             )  # find by name
+        self.bot.log.info("Admin logs enabled.")
         self.update.start()
 
     def splitLine(self, line: str):
@@ -42,13 +48,17 @@ class AdminLogHandler(commands.Cog):
     @tasks.loop(seconds=10)
     async def update(self):
         # Don't really know how performant this will be since it's opening 4 files -- set it to 10 seconds loop for now
-        files = glob.glob(self.logPath + "/*map.txt") + glob.glob(self.logPath + "/*cmd.txt") + glob.glob(self.logPath + "/*admin.txt") + glob.glob(self.logPath + "/*ClientActionLog.txt")
+        files = glob.glob(self.logPath + "/*map.txt") + glob.glob(self.logPath + "/*cmd.txt") + glob.glob(self.logPath + "/*admin.txt") + glob.glob(self.logPath + "/*ClientActionLog.txt") + glob.glob(self.logPath + "/*user.txt") + glob.glob(self.logPath + "/*pvp.txt") + glob.glob(self.logPath + "/*PerkLog.txt")
         if len(files) > 0:
             newTimestamps = []
             for file in files:
                 with FileReadBackwards(file) as f:
                     for line in f:
                         timestamp, message = self.splitLine(line)
+                        if "closed server." in message and not self.was_turned_off:
+                            await self.text_channel.send("Server was shutdown.")
+                            self.bot.log.info("Server closed..")
+                            self.was_turned_off = True
                         if timestamp > self.lastUpdateTimestamp:
                             newTimestamps.append(timestamp)
                             message = f'[{str(timestamp)}]: ' + message
