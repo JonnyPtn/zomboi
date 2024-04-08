9247a7a5bb4e1852135fca9633440ac906822422
JonnyPtn <jonathan.r.paton@googlemail.com>
2024-04-08 22:43:25 +0100

    Add cross-platform support for server management

--- Server.cs
+++ Server.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
+using System.Formats.Tar;
 using System.IO.Compression;
-using System.Net;
 using System.Runtime.InteropServices;
 
 namespace zomboi
@@ -8,7 +8,21 @@ namespace zomboi
     internal class Server
     {
         private static string serverPath = "server";
-        private static string serverStartPath = $"{serverPath}/StartServer64.bat";
+        private static string StartPath { get 
+        {
+            if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return $"{serverPath}/StartServer64.bat";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                return $"{serverPath}/StartServer.Command";
+            }
+            else
+            {
+                return $"{serverPath}/start-server.sh";
+            }
+        }}
         private static readonly Process serverProcess = new();
 
         public static bool IsRunning { get { return serverProcess.StartInfo.FileName.Length > 0 && !serverProcess.HasExited; } }
@@ -18,7 +32,7 @@ namespace zomboi
         public static List<Player> players = new List<Player>();
         public static bool Start()
         {
-            serverProcess.StartInfo.FileName = serverStartPath;
+            serverProcess.StartInfo.FileName = StartPath;
             serverProcess.StartInfo.RedirectStandardInput = true;
             serverProcess.Start();
             return IsRunning;
@@ -31,22 +45,61 @@ namespace zomboi
             await serverProcess.WaitForExitAsync();
         }
 
-        public static async Task Install()
+        private static async Task DownloadSteamCMD()
         {
-            var zipPath = "steamcmd.zip";
-            if (!File.Exists(zipPath))
+            var downloadFile = "steamcmd.file";
+            if (!File.Exists(downloadFile))
             {
-                using (var client = new WebClient())
+                using (var client = new HttpClient())
                 {
-                    client.DownloadFile("https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip", zipPath);
+                    string uri = "https://steamcdn-a.akamaihd.net/client/installer/steamcmd";
+
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        uri += ".zip";
+                    }
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                    {
+                        uri += "_osx.tar.gz";
+                    }
+                    else
+                    {
+                        uri += "_linux.tar.gz";
+                    }
+                    using (var s = await client.GetStreamAsync(uri))
+                    {
+                        using (var fs = new FileStream(downloadFile, FileMode.CreateNew))
+                        {
+                            await s.CopyToAsync(fs);
+                        }
+                    }
                 }
             }
 
             var unpackedPath = "steamcmd";
             if (!Directory.Exists(unpackedPath))
             {
-                ZipFile.ExtractToDirectory(zipPath, unpackedPath);
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    ZipFile.ExtractToDirectory(downloadFile, unpackedPath);
+                }
+                else
+                {
+                    Directory.CreateDirectory(unpackedPath);
+                    using (var fs = new FileStream(downloadFile, FileMode.Open))
+                    {
+                        using (var gz = new GZipStream(fs, CompressionMode.Decompress))
+                        {
+                            await TarFile.ExtractToDirectoryAsync(gz, unpackedPath, true);
+                        }
+                    }
+                }
             }
+        }
+
+        public static async Task Install()
+        {
+            await DownloadSteamCMD();
 
             Process process = new();
             process.StartInfo.FileName = "steamcmd/steamcmd";
@@ -55,11 +108,14 @@ namespace zomboi
             process.Start();
             await process.WaitForExitAsync();
 
-            // We don't want the batch file to wait for input, so remove any PAUSE
-            var pause = "PAUSE";
-            var file = File.ReadAllText(serverStartPath);
-            file = file.Replace(pause, "");
-            File.WriteAllText(serverStartPath, file);
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                // We don't want the batch file to wait for input, so remove any PAUSE
+                var pause = "PAUSE";
+                var file = File.ReadAllText(StartPath);
+                file = file.Replace(pause, "");
+                File.WriteAllText(StartPath, file);
+            }
         }
 
         public static async Task Create(string password)
